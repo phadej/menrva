158511716295a76d024d24f04c5acdea33228c35
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-07-09 10:37:36 +0300

    convinience methods: tuple and onSpread

--- src/convinience.js
+++ src/convinience.js
@@ -25,3 +25,37 @@ signal.Signal.prototype.log = function () {
     console.log.apply(console, logArgs);
   });
 };
+
+/**
+  #### signal.onSpread
+
+  > signal.onSpread (@ : Signal [a, b...], callback : a -> b ... -> void) : Unsubscriber
+
+  `onValue` with signal's tuple arguments spread.
+*/
+signal.Signal.prototype.onSpread = function (callback) {
+  return this.onValue(function (t) {
+    callback.apply(undefined, t);
+  });
+};
+
+/**
+  #### tuple
+
+  > tuple (x : Signal a, y : Signal b...) : Signal [a, b...]
+
+  Combine signals into tuple.
+*/
+function argsToArray() {
+  return Array.prototype.slice.call(arguments);
+}
+
+function tuple() {
+  var signals = Array.prototype.slice.call(arguments);
+  var args = signals.concat([argsToArray]);
+  return signal.combine.apply(signal, args);
+}
+
+module.exports = {
+  tuple: tuple,
+};
